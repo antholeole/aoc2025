840055e8ef40009f6c5f0e76be21824cd17f1f86
oleina <oleina@google.com>
2025-12-07 10:23:04 -0600

    day 7 pt 2

--- day7/day7.py
+++ day7/day7.py
@@ -1,21 +1,24 @@
 def solve(input: str) -> int:
-    beams_split = 0
-
     lines = input.splitlines()
-    prev_row_beams: set[int] = {lines[0].index("S")}
 
-    for line in lines[1:]:
-        this_row_beams: set[int] = set()
+    # from bottom up, if there's a
+    possibilties_at_column: dict[int, int] = {x: 1 for x in range(len(lines[0]))}
+
+    for line in reversed(lines[:-1]):
+        this_row_possibilties: dict[int, int] = {}
         for c, pos in enumerate(line):
-            # if there's no beam above us we can't do anything
-            if c not in prev_row_beams:
+            # if we aren't a splitter, then just carry up the possibilties below
+            if pos == ".":
+                this_row_possibilties[c] = possibilties_at_column[c]
                 continue
 
-            if pos == "^":
-                this_row_beams.add(c - 1)
-                this_row_beams.add(c + 1)
-                beams_split += 1
-            else:
-                this_row_beams.add(c)
-        prev_row_beams = this_row_beams
-    return beams_split
+            # if we are a splitter, its the possibilities of bottom left and
+            # bottom right summed.
+            this_row_possibilties[c] = (
+                possibilties_at_column[c - 1] + possibilties_at_column[c + 1]
+            )
+        possibilties_at_column = this_row_possibilties
+
+
+    # the solution is the number of possibilities at the start point.        
+    return possibilties_at_column[lines[0].index('S')]
